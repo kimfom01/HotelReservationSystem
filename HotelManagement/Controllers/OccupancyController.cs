f18cd7a544d2a0bcd893cec5728c48525e38067b
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-07-20 18:27:44 +0300

    Add occupancy controller and actions

---
+++ HotelManagement/Controllers/OccupancyController.cs
@@ -0,0 +1,77 @@
+﻿using HotelManagement.Models;
+using HotelManagement.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OccupancyController : ControllerBase
+{
+    private readonly Repository<Occupancy> _repository;
+
+    public OccupancyController(Repository<Occupancy> repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> GetOccupancy(int id)
+    {
+        var occupancy = await _repository.GetEntity(occ => occ.Id == id);
+
+        if (occupancy is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(occupancy);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(200)]
+    public async Task<IActionResult> GetOccupancies()
+    {
+        var occupancies = await _repository.GetEntities(occ => true);
+
+        return Ok(occupancies);
+    }
+
+    [HttpDelete("{id:int}")]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> DeleteOccupancy(int id)
+    {
+        await _repository.Delete(id);
+        int deltedCount = await _repository.SaveChanges();
+
+        if (deltedCount <= 0)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+
+    [HttpPut]
+    [ProducesResponseType(204)]
+    public async Task<IActionResult> UpdateOccupancy(Occupancy occupancy)
+    {
+        await _repository.Update(occupancy);
+        await _repository.SaveChanges();
+
+        return NoContent();
+    }
+
+    [HttpPost]
+    [ProducesResponseType(201)]
+    public async Task<IActionResult> PostOccupancy(Occupancy occupancy)
+    {
+        var added = await _repository.Add(occupancy);
+        await _repository.SaveChanges();
+
+        return CreatedAtAction(nameof(GetOccupancy), new { id = added.Id }, added);
+    }
+}
