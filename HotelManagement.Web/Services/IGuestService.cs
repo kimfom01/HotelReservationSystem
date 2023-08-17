68212a2196366d4a2679cfe8397041ecbb10f3b8
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-08-17 20:09:25 +0300

    Implement sample guest service with fetch guests

---
+++ HotelManagement.Web/Services/IGuestService.cs
@@ -0,0 +1,9 @@
+﻿using HotelManagement.Web.Models.Dtos;
+
+namespace HotelManagement.Web.Services;
+
+public interface IGuestService
+{
+    Task<IEnumerable<Guest>> FetchGuests();
+    Task<Guest> AddGuest(Guest guest);
+}
