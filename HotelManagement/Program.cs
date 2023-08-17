fccca2abe02269fc3bd09fa5af65aa0211cd7eff
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-08-17 19:05:33 +0300

    Add cors policy to allow all origins

--- HotelManagement/Program.cs
+++ HotelManagement/Program.cs
@@ -2,15 +2,22 @@ using HotelManagement.Data;
 using HotelManagement.Repositories;
 using Microsoft.EntityFrameworkCore;
 
+var corsPolicy = "any origin";
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: corsPolicy,
+        policy => policy.WithOrigins().AllowAnyOrigin());
+});
 builder.Services.AddDbContext<Context>(options =>
 {
-    //options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-    options.UseInMemoryDatabase("TemporaryDb"); // For testing purpose... will revert to postgres when needed
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    //options.UseInMemoryDatabase("TemporaryDb"); // For testing purpose... will revert to postgres when needed
 
 });
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
@@ -24,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(corsPolicy);
 
 app.UseAuthorization();
 
