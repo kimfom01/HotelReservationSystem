50b41388859d163ceb5f7ef0cb19c0bf22e83243
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-08-17 19:56:27 +0300

    Add missing properties of meal model

--- HotelManagement/Models/Meal.cs
+++ HotelManagement/Models/Meal.cs
@@ -5,4 +5,7 @@ public class Meal
     public int Id { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
+    public decimal MealPrice { get; set; }
+    public int HotelId { get; set; }
+    public Hotel? Hotel { get; set; }
 }
