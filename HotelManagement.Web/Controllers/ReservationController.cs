4eebeb914db3de27f330da040eab79a3acf037c7
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-08-18 18:21:19 +0300

    Add Reservation controller with index view

---
+++ HotelManagement.Web/Controllers/ReservationController.cs
@@ -0,0 +1,22 @@
+﻿using HotelManagement.Web.Models.Dtos;
+using HotelManagement.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Web.Controllers;
+
+public class ReservationController : Controller
+{
+    private readonly IGenericApiService<Reservation> _reservationService;
+
+    public ReservationController(IGenericApiService<Reservation> reservationService)
+    {
+        _reservationService = reservationService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var reservations = await _reservationService.FetchEntities();
+
+        return View(reservations);
+    }
+}
