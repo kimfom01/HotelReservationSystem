3ccb7b985e39d1e13032e65903ed05abe3d6d647
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-08-22 15:55:20 +0300

    Add date of birth and middle name to application user

--- HotelManagement.Web/Data/ApplicationUser.cs
+++ HotelManagement.Web/Data/ApplicationUser.cs
@@ -7,5 +7,7 @@ public class ApplicationUser : IdentityUser
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string MiddleName { get; set; }
+    public DateTime DateOfBirth { get; set; }
 }
 
