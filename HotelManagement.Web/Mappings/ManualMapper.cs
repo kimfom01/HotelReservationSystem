d3a12824f2ab947155e0434d1421707a31374018
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-08-22 15:56:06 +0300

    Map user model to guest

---
+++ HotelManagement.Web/Mappings/ManualMapper.cs
@@ -0,0 +1,22 @@
+﻿using HotelManagement.Web.Data;
+using HotelManagement.Web.Models.Dtos;
+
+namespace HotelManagement.Web.Mappings;
+
+public class ManualMapper
+{
+    public Guest MapUserToGuest(ApplicationUser user)
+    {
+        var guest = new Guest
+        {
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            MiddleName = user.MiddleName,
+            Email = user.Email,
+            Phone = user.PhoneNumber,
+            DateOfBirth = user.DateOfBirth
+        };
+
+        return guest;
+    }
+}
