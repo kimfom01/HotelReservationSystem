67a91797e6bce4dcf434e524caa6c2c8ace6d432
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-08-23 21:42:12 +0300

    Implement reservation actions

--- HotelManagement.Web/Controllers/ReservationController.cs
+++ HotelManagement.Web/Controllers/ReservationController.cs
@@ -1,19 +1,96 @@
-﻿using HotelManagement.Web.Models.Dtos;
+﻿using HotelManagement.Web.Data;
+using HotelManagement.Web.Models.Dtos;
+using HotelManagement.Web.Models.ViewModels;
 using HotelManagement.Web.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HotelManagement.Web.Controllers;
 
+[Authorize]
 public class ReservationController : Controller
 {
     private readonly IGenericApiService<Reservation> _reservationService;
+    private readonly IGenericApiService<Hotel> _hotelService;
+    private readonly IRoomApiService _roomService;
+    private readonly IGuestApiService _guestService;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public ReservationController(IGenericApiService<Reservation> reservationService)
+    public ReservationController(
+        IGenericApiService<Reservation> reservationService,
+        IGenericApiService<Hotel> hotelService,
+        IRoomApiService roomService,
+        IGuestApiService guestService,
+        UserManager<ApplicationUser> userManager)
     {
         _reservationService = reservationService;
+        _hotelService = hotelService;
+        _roomService = roomService;
+        _guestService = guestService;
+        _userManager = userManager;
     }
 
     public async Task<IActionResult> Index()
+    {
+        var hotels = await _hotelService.FetchEntities();
+
+        var reservationViewModel = new ReservationViewModel
+        {
+            Hotels = new SelectList(hotels, "Id", "Name")
+        };
+
+        return View(reservationViewModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(ReservationViewModel reservationViewModel)
+    {
+        var room = await _roomService.FetchRoomByHotelId(reservationViewModel.HotelId, reservationViewModel.NumberOfGuests);
+
+        var tax = 100M; //Don't know where these data come from lol
+        var discount = 0.0;
+
+        int guestId = await GetGuestId();
+
+        var reservation = new Reservation
+        {
+            Date = reservationViewModel.Date,
+            CheckIn = reservationViewModel.CheckIn,
+            CheckOut = reservationViewModel.CheckOut,
+            GuestId = guestId,
+            HotelId = reservationViewModel.HotelId,
+            NumberOfGuests = reservationViewModel.NumberOfGuests,
+            Price = room!.RoomPrice,
+            Tax = tax,
+            Discount = discount,
+        };
+
+        await _reservationService.AddEntity(reservation);
+
+        return RedirectToAction(nameof(ViewReservationList));
+    }
+
+    private async Task<int> GetGuestId()
+    {
+        var user = await _userManager.GetUserAsync(User);
+
+        if (user is null)
+        {
+            return default;
+        }
+
+        var emailAddress = user.Email;
+
+        var guest = await _guestService.FetchGuestByEmailAddress(emailAddress!);
+
+        var guestId = guest!.Id;
+
+        return guestId;
+    }
+
+    public async Task<IActionResult> ViewReservationList()
     {
         var reservations = await _reservationService.FetchEntities();
 
