35e908e84a7bdebcbd31309a2bda2fcc055c1e12
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-08-23 21:38:45 +0300

    Create a reservation view model to move data from front page to backend

---
+++ HotelManagement.Web/Models/ViewModels/ReservationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HotelManagement.Web.Models.ViewModels;
+
+public class ReservationViewModel
+{
+    public SelectList? Hotels { get; set; }
+    public DateTime CheckIn { get; set; }
+    public DateTime CheckOut { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
+    public int HotelId { get; set; }
+    public int NumberOfGuests { get; set; }
+}
