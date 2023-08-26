748902bd59d0cda063a4d13240cded6fda3eabbd
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-08-26 04:17:56 +0300

    Implement hotel controller with index, add and manage hotel actions

---
+++ HotelManagement.Web/Controllers/HotelController.cs
@@ -0,0 +1,74 @@
+﻿using HotelManagement.Web.Models;
+using HotelManagement.Web.Models.Dtos;
+using HotelManagement.Web.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Web.Controllers;
+
+[Authorize(Roles = nameof(Roles.SystemAdmin))]
+public class HotelController : Controller
+{
+    private readonly IGenericApiService<Hotel> _hotelService;
+
+    public HotelController(IGenericApiService<Hotel> hotelService)
+    {
+        _hotelService = hotelService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var hotels = await _hotelService.FetchEntities();
+
+        return View(hotels);
+    }
+
+    public IActionResult AddHotel()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddHotel(Hotel hotel)
+    {
+        var addedHotel = await _hotelService.AddEntity(hotel);
+
+        if (addedHotel is null)
+        {
+            return View();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> ManageHotel(int id)
+    {
+        var hotel = await _hotelService.FetchEntity(id);
+
+        if (hotel is null)
+        {
+            return NotFound($"Hotel with id = {id} cannot be found");
+        }
+
+        return View(hotel);
+    }
+
+    [HttpPost("{id:int}")]
+    public async Task<IActionResult> ManageHotel(Hotel newHotelInfo, int id)
+    {
+        var hotel = await _hotelService.FetchEntity(id);
+
+        hotel.Name = newHotelInfo.Name;
+        hotel.Location = newHotelInfo.Location;
+
+        var success = await _hotelService.UpdateEntity(hotel, id);
+
+        if (success)
+        {
+            return View();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+}
