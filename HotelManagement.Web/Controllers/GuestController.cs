e659c3f25fb1e5dec3c4be64964a857e320f1910
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-09-01 20:04:33 +0300

    Add guest controller with index page

---
+++ HotelManagement.Web/Controllers/GuestController.cs
@@ -0,0 +1,21 @@
+﻿using HotelManagement.Web.Models.Dtos;
+using HotelManagement.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Web.Controllers;
+public class GuestController : Controller
+{
+    private readonly IGenericApiService<Guest> _guestService;
+
+    public GuestController(IGenericApiService<Guest> guestService)
+    {
+        _guestService = guestService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var guests = await _guestService.FetchEntities();
+
+        return View(guests);
+    }
+}
