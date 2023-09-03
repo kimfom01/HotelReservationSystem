d346207724e9d9b039310a2e2af03e25635fe4b5
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-09-03 19:12:48 +0300

    Implement manage room status

--- HotelManagement.Web/Controllers/RoomStatusController.cs
+++ HotelManagement.Web/Controllers/RoomStatusController.cs
@@ -2,6 +2,7 @@
 using HotelManagement.Web.Models.ViewModels;
 using HotelManagement.Web.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HotelManagement.Web.Controllers;
 
@@ -45,5 +46,89 @@ public class RoomStatusController : Controller
         }
 
         return View(roomStatusViewModels);
+    }
+
+    //public async Task<IActionResult> AddRoomStatus()
+    //{
+    //    var rooms = await _roomService.FetchEntities();
+    //    var guests = await _guestService.FetchEntities();
+
+    //    var roomServiceVM = new RoomStatusViewModel
+    //    {
+    //        Rooms = new SelectList(rooms, "Id", "RoomType"),
+    //        Guests = new SelectList(guests, "Id", "FirstName")
+    //    };
+
+    //    return View(roomServiceVM);
+    //}
+
+    //[HttpPost]
+    //public async Task<IActionResult> AddRoomStatus(RoomStatusViewModel roomStatusViewModel)
+    //{
+    //    var guest = await _guestService.FetchEntity(roomStatusViewModel.GuestId);
+
+    //    var roomStatus = new RoomStatus
+    //    {
+    //        RoomId = roomStatusViewModel.RoomId,
+    //        GuestId = roomStatusViewModel.GuestId,
+    //        ReservationId = roomStatusViewModel.ReservationId,
+    //        CheckIn = roomStatusViewModel.CheckIn,
+    //        CheckOut = roomStatusViewModel.CheckOut
+    //    };
+
+    //    var addedStatus = await _roomStatusService.AddEntity(roomStatus);
+
+    //    if (addedStatus is null)
+    //    {
+    //        return View();
+    //    }
+
+    //    return RedirectToAction(nameof(Index));
+    //}
+
+    public async Task<IActionResult> ManageRoomStatus(int statusId)
+    {
+        var roomStatus = await _roomStatusService.FetchEntity(statusId);
+
+        if (roomStatus is null)
+        {
+            return View();
+        }
+
+        var rooms = await _roomService.FetchEntities();
+
+        var roomStatusVM = new RoomStatusViewModel
+        {
+            CheckIn = roomStatus.CheckIn,
+            CheckOut = roomStatus.CheckOut,
+            Rooms = new SelectList(rooms, "Id", "RoomType"),
+            RoomId = roomStatus.RoomId
+        };
+
+        return View(roomStatusVM);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ManageRoomStatus(RoomStatusViewModel roomStatusViewModel, int statusId)
+    {
+        var roomStatus = await _roomStatusService.FetchEntity(statusId);
+
+        if (roomStatus is null)
+        {
+            return NotFound($"Room Status with id = {statusId} not found");
+        }
+
+        roomStatus.RoomId = roomStatusViewModel.RoomId;
+        roomStatus.CheckIn = roomStatusViewModel.CheckIn;
+        roomStatus.CheckOut = roomStatusViewModel.CheckOut;
+
+        var success = await _roomStatusService.UpdateEntity(roomStatus);
+
+        if (!success)
+        {
+            return View();
+        }
+
+        return RedirectToAction(nameof(Index));
     }
 }
