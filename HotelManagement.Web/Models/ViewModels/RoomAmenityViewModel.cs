0a2d622a83e2198ed4c273bce95453087ecc05da
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-09-03 15:23:01 +0300

    Add room amenity controller with index, add and manage (edit)

---
+++ HotelManagement.Web/Models/ViewModels/RoomAmenityViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HotelManagement.Web.Models.ViewModels;
+
+public class RoomAmenityViewModel
+{
+    public int AmenityId { get; set; }
+    public string RoomType { get; set; }
+    public string Name { get; set; }
+    public int RoomNumber { get; set; }
+    public SelectList? Rooms { get; set; }
+    public int RoomId { get; set; }
+}
