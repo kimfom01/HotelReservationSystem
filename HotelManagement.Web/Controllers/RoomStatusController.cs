e6dfe6df814cad384e391e06635410e062ff11cb
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-09-03 16:15:47 +0300

    Add room status controller with index view and action

--- HotelManagement.Web/Controllers/RoomStatusController.cs
+++ HotelManagement.Web/Controllers/RoomStatusController.cs
@@ -1,4 +1,5 @@
 ﻿using HotelManagement.Web.Models.Dtos;
+using HotelManagement.Web.Models.ViewModels;
 using HotelManagement.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,14 +8,42 @@ namespace HotelManagement.Web.Controllers;
 public class RoomStatusController : Controller
 {
     private readonly IGenericApiService<RoomStatus> _roomStatusService;
+    private readonly IGenericApiService<Room> _roomService;
+    private readonly IGenericApiService<Guest> _guestService;
 
-    public RoomStatusController(IGenericApiService<RoomStatus> roomStatusService)
+    public RoomStatusController(
+        IGenericApiService<RoomStatus> roomStatusService,
+        IGenericApiService<Room> roomService,
+        IGenericApiService<Guest> guestService)
     {
         _roomStatusService = roomStatusService;
+        _roomService = roomService;
+        _guestService = guestService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var roomStatuses = await _roomStatusService.FetchEntities();
+
+        var roomStatusViewModels = new List<RoomStatusViewModel>();
+
+        foreach (var status in roomStatuses)
+        {
+            var room = await _roomService.FetchEntity(status.RoomId);
+            var guest = await _guestService.FetchEntity(status.GuestId);
+
+            var roomStatusVM = new RoomStatusViewModel
+            {
+                RoomStatusId = status.Id,
+                RoomType = room.RoomType,
+                GuestName = guest.FirstName,
+                CheckIn = status.CheckIn,
+                CheckOut = status.CheckOut
+            };
+
+            roomStatusViewModels.Add(roomStatusVM);
+        }
+
+        return View(roomStatusViewModels);
     }
 }
