5bc4424a5d310b2e2f27a9120ed7b79b3dc08e1d
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-09-10 23:18:59 +0300

    Fix typo in model property

--- DataAccess/Models/ReservationRoom.cs
+++ DataAccess/Models/ReservationRoom.cs
@@ -3,7 +3,7 @@
 public class ReservationRoom
 {
     public int Id { get; set; }
-    public int Reservation_Id { get; set; }
+    public int ReservationId { get; set; }
     public Reservation? Reservation { get; set; }
     public int RoomId { get; set; }
     public Room? Room { get; set; }
