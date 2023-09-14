b97a894f04bab87b97a5e3951b0c7e0d7770803a
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-09-14 12:07:20 +0300

    Use auto mapper to map models to dtos

---
+++ Api/Mapping/AutoMapping.cs
@@ -0,0 +1,25 @@
+﻿using Api.Dtos;
+using AutoMapper;
+using DataAccess.Models;
+
+namespace Api.Mapping;
+
+public class AutoMapping : Profile
+{
+    public AutoMapping()
+    {
+        CreateMap<Employee, EmployeeDto>().ReverseMap();
+        CreateMap<Guest, GuestDto>().ReverseMap();
+        CreateMap<HotelAmenity, HotelAmenityDto>().ReverseMap();
+        CreateMap<Hotel, HotelDto>().ReverseMap();
+        CreateMap<Maintenance, MaintenanceDto>().ReverseMap();
+        CreateMap<Meal, MealDto>().ReverseMap();
+        CreateMap<Pricing, PricingDto>().ReverseMap();
+        CreateMap<Reservation, ReservationDto>().ReverseMap();
+        CreateMap<ReservationRoom, ReservationRoomDto>().ReverseMap();
+        CreateMap<RoomAmenity, RoomAmenityDto>().ReverseMap();
+        CreateMap<Room, RoomDto>().ReverseMap();
+        CreateMap<RoomStatus, RoomStatusDto>().ReverseMap();
+        CreateMap<Service, ServiceDto>().ReverseMap();
+    }
+}
