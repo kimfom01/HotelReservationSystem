2ce038c149f8b401414047260ea805ad0f7ed3e7
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-09-26 23:23:12 +0300

    Remove condition for migration

--- src/Frontend/Web/Program.cs
+++ src/Frontend/Web/Program.cs
@@ -41,10 +41,7 @@ using var scope = app.Services.CreateScope();
 var serviceProvider = scope.ServiceProvider;
 var context = serviceProvider.GetRequiredService<HotelManagementWebContext>();
 await context.Database.EnsureCreatedAsync();
-if (app.Environment.IsDevelopment())
-{
-    await context.Database.MigrateAsync();
-}
+await context.Database.MigrateAsync();
 await ContextSeed.SeedRolesAsync(serviceProvider);
 await ContextSeed.SeedSuperAdminAsync(serviceProvider);
 
