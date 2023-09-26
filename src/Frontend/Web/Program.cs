2d58dd7cfe168b16e6f78fa9f0ec42eea547a022
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-09-26 16:18:32 +0300

    Separate local development configuration from deployement

--- src/Frontend/Web/Program.cs
+++ src/Frontend/Web/Program.cs
@@ -41,7 +41,10 @@ using var scope = app.Services.CreateScope();
 var serviceProvider = scope.ServiceProvider;
 var context = serviceProvider.GetRequiredService<HotelManagementWebContext>();
 await context.Database.EnsureCreatedAsync();
-await context.Database.MigrateAsync();
+if (app.Environment.IsDevelopment())
+{
+    await context.Database.MigrateAsync();
+}
 await ContextSeed.SeedRolesAsync(serviceProvider);
 await ContextSeed.SeedSuperAdminAsync(serviceProvider);
 
