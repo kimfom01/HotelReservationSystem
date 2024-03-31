76620662eabe20f08e743c6a1ba097e50ffd0bf4
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-04-01 02:03:54 +0300

    Add statuses for reservation

---
+++ HotelBackend/HotelBackend.ReservationService/Models/enums/PaymentStatus.cs
@@ -0,0 +1,8 @@
+namespace HotelBackend.ReservationService.Models.enums;
+
+public enum PaymentStatus
+{
+    PAID,
+    PENDING,
+    REFUNDED
+}
---
+++ HotelBackend/HotelBackend.ReservationService/Models/enums/ReservationStatus.cs
@@ -0,0 +1,10 @@
+namespace HotelBackend.ReservationService.Models.enums;
+
+public enum ReservationStatus
+{
+    PENDING,
+    CONFIRMED,
+    CANCELLED,
+    CHECKEDIN,
+    CHECKEDOUT
+}
