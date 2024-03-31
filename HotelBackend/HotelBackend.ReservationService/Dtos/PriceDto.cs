f420f8e46a2157885c7c57fdff0598d0593b22fa
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-04-01 01:56:05 +0300

    Rename pricing to price

---
+++ HotelBackend/HotelBackend.ReservationService/Dtos/PriceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelBackend.ReservationService.Dtos;
+
+public class PriceDto
+{
+    public Guid Id { get; set; }
+    public decimal Value { get; set; }
+    public Guid RoomTypeId { get; set; }
+}
---
+++ HotelBackend/HotelBackend.ReservationService/Models/Price.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelBackend.ReservationService.Models;
+
+public class Price
+{
+    public Guid Id { get; set; }
+    public decimal Value { get; set; }
+    public Guid RoomTypeId { get; set; }
+    public RoomType? RoomType { get; set; }
+}
