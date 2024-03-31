b2adbe3462b3ccac4b130cc927c66ec7b6d3fcfc
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-04-01 02:03:32 +0300

    Add room type

---
+++ HotelBackend/HotelBackend.ReservationService/Models/RoomType.cs
@@ -0,0 +1,12 @@
+namespace HotelBackend.ReservationService.Models;
+
+public class RoomType
+{
+    public Guid Id { get; set; }
+    public string Type { get; set; }
+    public int Capacity { get; set; }
+    public string Description { get; set; }
+    public Price? Price { get; set; }
+
+    public IEnumerable<Room>? Rooms { get; set; } // ????????????
+}
