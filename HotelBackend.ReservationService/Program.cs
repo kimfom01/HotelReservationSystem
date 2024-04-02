87956d485378eb6f9ff4b732f4a58ed1d468d60c
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-04-02 23:45:22 +0300

    Add queue to service registry

--- HotelBackend.ReservationService/Program.cs
+++ HotelBackend.ReservationService/Program.cs
@@ -1,19 +1,26 @@
 using System.Reflection;
+using System.Text.Json.Serialization;
 using HotelBackend.ReservationService.Data;
+using HotelBackend.ReservationService.Infrastructure;
 using HotelBackend.ReservationService.Repositories;
 using HotelBackend.ReservationService.Repositories.Implementations;
 using HotelBackend.ReservationService.Services;
 using HotelBackend.ReservationService.Services.Implementations;
 using HotelBackend.ReservationService.Utils;
 using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
 const string corsPolicy = "any origin";
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<RabbitMqOption>(builder.Configuration.GetSection(nameof(RabbitMqOption)));
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: corsPolicy, policy => policy.WithOrigins().AllowAnyOrigin());
@@ -37,6 +44,8 @@ builder.Services.AddScoped<IReservationService, ReservationService>();
 builder.Services.AddScoped<IRoomService, RoomService>();
 builder.Services.AddScoped<IHotelService, HotelService>();
 builder.Services.AddScoped<IPricingService, PricingService>();
+builder.Services.AddSingleton<IConnectionFactory, ConnectionFactory>();
+builder.Services.AddScoped<QueueService>();
 
 var app = builder.Build();
 
